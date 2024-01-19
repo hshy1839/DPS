92466d8a38c5a0c9897b6260f33ba0430ec318c5
hshy1839 <hongjeongmin1839@gmail.com>
2024-01-20 00:24:37 +0800

    ScrollView 추가

--- app/src/main/java/com/example/dps/MainActivity.kt
+++ app/src/main/java/com/example/dps/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.dps
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val loginButton = findViewById<Button>(R.id.loginButton)
+        val loginButton = findViewById<ImageView>(R.id.loginButton)
         loginButton.setOnClickListener { // LoginActivity로 전환
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
