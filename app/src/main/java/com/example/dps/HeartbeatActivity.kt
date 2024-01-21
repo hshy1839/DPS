cdb7160128667a32029c7298268fa2841c560726
hshy1839 <hongjeongmin1839@gmail.com>
2024-01-21 23:17:55 +0800

    heartbeat 페이지 메뉴 추가 요일별 심박수 추가

--- app/src/main/java/com/example/dps/HeartbeatActivity.kt
+++ app/src/main/java/com/example/dps/HeartbeatActivity.kt
@@ -1,19 +1,34 @@
 package com.example.dps
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.google.android.material.navigation.NavigationView
 
 class HeartbeatActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_heartbeat)
+
+        val loginButton = findViewById<ImageView>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this@HeartbeatActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         val lineChart = findViewById<LineChart>(R.id.lineChart)
         setupLineChart(lineChart)
@@ -26,6 +41,43 @@ class HeartbeatActivity : AppCompatActivity() {
         entries.add(Entry(4f, 88f))
         entries.add(Entry(5f, 95f))
         addDataToLineChart(lineChart, entries)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        // 토글 버튼을 추가하여 메뉴가 열리고 닫히도록 함
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // 네비게이션 메뉴 아이템 클릭 리스너 설정
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_item1 -> {
+                    // Menu 1 선택 시의 동작
+                    showToast("Select Menu 1")
+                }
+                R.id.nav_item2 -> {
+                    // Menu 2 선택 시의 동작
+                    showToast("Select Menu 2")
+                }
+                R.id.nav_item3 -> {
+                    // Menu 3 선택 시의 동작
+                    showToast("Select Menu 3")
+                }
+            }
+            // 메뉴를 선택한 후에는 Drawer를 닫아줌
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+        val menuButton = findViewById<ImageView>(R.id.menuButton_heartbeat)
+        menuButton.setOnClickListener {
+            // 메뉴 버튼을 클릭하면 Navigation Drawer를 열도록 함
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
     private fun setupLineChart(lineChart: LineChart) {
         // LineChart 설정
@@ -45,6 +97,22 @@ class HeartbeatActivity : AppCompatActivity() {
 
         val rightAxis = lineChart.axisRight
         rightAxis.isEnabled = false
+
+    }
+
+    override fun onBackPressed() {
+        // 뒤로가기 버튼을 누를 때
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            // 만약 Navigation Drawer가 열려 있다면, 닫기
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            // 그렇지 않으면 기본 동작 수행
+            super.onBackPressed()
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun addDataToLineChart(lineChart: LineChart, entries: List<Entry>) {
