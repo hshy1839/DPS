3c1b9b949d63d533c7ff465e89bcaa36864ab24f
hshy1839 <hongjeongmin1839@gmail.com>
2024-03-04 21:37:01 +0800

    footer 및 타이틀 변경

--- app/src/main/java/com/example/dps/MainActivity.kt
+++ app/src/main/java/com/example/dps/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dps
 
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
